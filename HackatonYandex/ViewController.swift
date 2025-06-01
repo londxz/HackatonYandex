1c5e2b42f13331d319845a6476d355cc49dbbd72
Rodion Kholodov <rodionn39@gmail.com>
2025-06-01 12:22:41 +0300

    made launchScreen

---
+++ HackatonYandex/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HackatonYandex
+//
+//  Created by Родион Холодов on 01.06.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemPink
+    }
+
+
+}
+
