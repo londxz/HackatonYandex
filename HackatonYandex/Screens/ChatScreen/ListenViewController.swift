1af454dc29ed5fa403fe4db6ecd52312b9fce7ed
Rodion Kholodov <rodionn39@gmail.com>
2025-06-02 02:59:03 +0300

    edit structure

--- HackatonYandex/Screens/ChatScreen/ListenViewController.swift
+++ HackatonYandex/Screens/ChatScreen/ListenViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 class ListenViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .chatBackground
+        view.backgroundColor = .backgroundYandex
 
         let label = UILabel()
         label.text = "Режим слушания"
